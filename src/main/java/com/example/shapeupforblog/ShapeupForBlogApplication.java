096968ede9410a0dbaf58f94a6a4868f4975faa8
hxnsxxm <hansaemj20@gmail.com>
2024-04-08 22:37:55 +0900

    first commit

---
+++ src/main/java/com/example/shapeupforblog/ShapeupForBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.shapeupforblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeupForBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShapeupForBlogApplication.class, args);
+    }
+
+}
